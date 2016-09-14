6c35b71cb459df565443ef2b61d8c6e142e5d7a3
Stephen Emery and Vanessa Palacios Sharma <stephen@>
2016-09-14 13:53:10 -0700

    Finish backend with junit tests.

---
+++ src/main/java/MusicOrg.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicOrg {
+  private String mAlbum;
+  private String mArtist;
+  private static List<MusicOrg> instances = new ArrayList<MusicOrg>();
+  private int mId;
+
+  public MusicOrg (String album, String artist) {
+    mAlbum = album;
+    mArtist = artist;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public String getAlbum() {
+    return mAlbum;
+  }
+
+  public String getArtist() {
+    return mArtist;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static MusicOrg find(int id) {
+    return instances.get(id - 1);
+  }
+
+  public static List<MusicOrg> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+}
---
+++ src/test/java/MusicOrgTest.java
@@ -0,0 +1,52 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MusicOrgTest {
+
+  @Test
+  public void MusicOrg_instatiatesCorrectly_true() {
+    MusicOrg newMusic = new MusicOrg("Album Name", "Artist Name");
+    assertEquals(true, newMusic instanceof MusicOrg);
+  }
+
+  @Test
+  public void MusicOrg_instatiatesWithAlbum_String() {
+    MusicOrg newMusic = new MusicOrg("Album Name", "Artist Name");
+    assertEquals("Album Name", newMusic.getAlbum());
+  }
+
+  @Test
+  public void MusicOrg_instatiatesWithArtist_String() {
+    MusicOrg newMusic = new MusicOrg("Album Name", "Artist Name");
+    assertEquals("Artist Name", newMusic.getArtist());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfMusicOrg_true() {
+    MusicOrg firstAlbum = new MusicOrg("Album 1", "Artist 1");
+    MusicOrg secondAlbum = new MusicOrg("Album 2", "Artist 2");
+    assertEquals(true, MusicOrg.all().contains(firstAlbum));
+    assertEquals(true, MusicOrg.all().contains(secondAlbum));
+  }
+
+  @Test
+  public void clear_emptiesAllMusicOrgFromArrayList_0() {
+    MusicOrg firstAlbum = new MusicOrg("Album 1", "Artist 1");
+    MusicOrg.clear();
+    assertEquals(MusicOrg.all().size(), 0);
+  }
+
+  @Test
+  public void getId_musicOrgInstantiateWithAnID_1() {
+    MusicOrg.clear();
+    MusicOrg firstAlbum = new MusicOrg("Album 1", "Artist 1");
+    assertEquals(1, firstAlbum.getId());
+  }
+
+  @Test
+  public void find_returnsMusicOrgWithSameId_secondAlbum() {
+    MusicOrg firstAlbum = new MusicOrg("Album 1", "Artist 1");
+    MusicOrg secondAlbum = new MusicOrg("Album 2", "Artist 2");
+    assertEquals(MusicOrg.find(secondAlbum.getId()), secondAlbum);
+  }
+}
